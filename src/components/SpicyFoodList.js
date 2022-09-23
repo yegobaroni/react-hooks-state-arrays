71c790638964dd77065b217065889572a7f1eaf0
yego <yegokipkemoi254@gmail.com>
2022-09-23 17:41:07 +0300

    Done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,19 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
   }
 
+  const handleLiClick = (id) => {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {...food,heatLevel: food.heatLevel + 1,};
+      }else {return food;}
+    });
+    setFoods(newFoodArray);
+  }
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
